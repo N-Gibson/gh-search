3554b2e7cd1b43e802c28619c1b441f91b8d4ca0
Noah Gibson <noah.gibson99@gmail.com>
2020-02-18 16:22:00 -0700

    Implements search on click

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './SearchResults.css';
+import { searchRepositories } from '../../apiCalls';
 
 class SearchResults extends Component {
   constructor() {
     super();
     this.state = {
       userInput: '',
+      foundRepos: [],
+      error: ''
     }
   }
 
@@ -12,11 +16,19 @@ class SearchResults extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
+  searchRepos = async () => {
+    if(this.state.userInput) {
+      const repositories = await searchRepositories(this.state.userInput);
+      this.setState({ foundRepos: repositories.items });
+    }
+  }
+
   render() {
+    console.log(this.state.foundRepos);
     return (
       <div className="SearchResults">
         <input type='text' name='userInput' value={this.state.userInput} onChange={this.handleChange}></input>
-        <button>Search</button>
+        <button onClick={this.searchRepos}>Search</button>
       </div>
     )
   }

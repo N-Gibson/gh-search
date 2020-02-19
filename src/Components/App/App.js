02ba708dc747ab668991a0ab67ecbc2fbb1857bf
Noah Gibson <noah.gibson99@gmail.com>
2020-02-18 21:35:05 -0700

    Add sorting by star and filter by language functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserInteractions from '../UserInteractions/UserInteractions';
 import { Route } from 'react-router-dom';
 import './App.css';
--- src/Components/UserInteractions/UserInteractions.js
+++ src/Components/UserInteractions/UserInteractions.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { searchRepositories } from '../../apiCalls'; 
 import './UserInteractions.css';
-import SearchResults from '../SearchResults/SearchResults';
 import Repositories from '../Repositories/Repositories';
 import Repository from '../Repository/Repository';
 import { Route } from 'react-router-dom';
@@ -10,8 +9,9 @@ class UserInteractions extends Component {
   constructor() {
     super();
     this.state = {
-      userInput: '',
+      userInput: 'react',
       foundRepos: [],
+      languageInput: 'JavaScript',
     }
   }
 
@@ -26,20 +26,40 @@ class UserInteractions extends Component {
     }
   }
 
+  filterByLanguage = () => {
+    const languageFilter = this.state.foundRepos.filter(repo => repo.language === this.state.languageInput);
+    this.setState({ foundRepos: languageFilter });
+  }
+
+  sortByStars = (order) => {
+    let starSort;
+    console.log(this.state.foundRepos[0])
+
+    if(order === 'most') {
+      starSort = this.state.foundRepos.sort((a, b) => {
+        return a.stargazers_count - b.stargazers_count;
+      });
+      this.setState({ foundRepos: starSort });
+    } else {
+      starSort = this.state.foundRepos.sort((a, b) => {
+        return b.stargazers_count - a.stargazers_count;
+      });
+      this.setState({ foundRepos: starSort });
+    }
+  }
+
   render() {
     let foundRepositories = this.state.foundRepos.map(repo => <Repositories data={repo} key={repo.id}/>)
+
     return (
       <div className="user-interactions">
         <input type="text" name="userInput" value={this.state.userInput} onChange={this.handleChange}></input>
         <button onClick={this.searchRepos}>Search</button>
+        <input type="text" name="languageInput" value={this.state.languageInput} onChange={this.handleChange}></input>
+        <button onClick={this.filterByLanguage}>Search Language</button>
+        <button onClick={() => this.sortByStars('most')}>Most Stars</button>
+        <button onClick={() => this.sortByStars('least')}>Least Stars</button>
         <div>{foundRepositories}</div>
-        <Route exact path="/repository/:id" render={({ match }) => {
-          console.log('anytthisndkgfasdgf')
-          console.log(match);
-          const { id } = match.params;
-          let selectedRepo = this.state.foundRepos.find(repo => repo.id === id);
-          return <Repository data={selectedRepo} />}
-        } />
       </div>
     )
   }

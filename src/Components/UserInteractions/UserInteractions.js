e010c337eedbd355b1859c2bb374da15446c5086
Noah Gibson <noah.gibson99@gmail.com>
2020-02-18 19:57:07 -0700

    Add component layout user interactions

---
+++ src/Components/UserInteractions/UserInteractions.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './UserInteractions.css';
+
+class UserInteractions extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default UserInteractions;

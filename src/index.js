3e7bb379d5da4aa4227bab8b8dc8b74035a62fbc
Noah Gibson <noah.gibson99@gmail.com>
2020-02-18 22:03:57 -0700

    Add provider store to index js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './Components/App/App';
 import { HashRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './Reducers/index';
+
+const store = createStore(rootReducer, compose(composeWithDevTools()));
 
 ReactDOM.render(
-<HashRouter>
-  <App />
-</HashRouter>, document.getElementById('root'));
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
